f9850e3f7541d31de74f52d3ac81054f4ad46c19
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-06-23 14:54:15 +0300

    выделенные вспомогательные методы и классы_редакт1

--- test_add_group.py
+++ test_add_group.py
@@ -24,14 +24,14 @@ class TestAddGroup(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    # def test_add_empty_group(self):
-    #     wd = self.wd
-    #     self.open_home_page(wd)
-    #     self.login(wd, username="admin", password="secret")
-    #     self.open_groups_page(wd)
-    #     self.create_group(wd, Group(name="", header="", footer=""))
-    #     self.return_to_groups_page(wd)
-    #     self.logout(wd)
+    def test_add_empty_group(self):
+         wd = self.wd
+         self.open_home_page(wd)
+         self.login(wd, username="admin", password="secret")
+         self.open_groups_page(wd)
+         self.create_group(wd, Group(name="", header="", footer=""))
+         self.return_to_groups_page(wd)
+         self.logout(wd)
 
     def logout(self, wd):
         # logout
