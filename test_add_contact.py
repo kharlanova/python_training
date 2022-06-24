23c223169c729247f060d88cd84cbd1145630f67
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-06-24 09:45:20 +0300

    выделенные вспомогательные методы и классы_редакт2

--- test_add_contact.py
+++ test_add_contact.py
@@ -18,22 +18,35 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page(wd)
+        self.login(wd)
+        self.add_new_contact(wd)
+        self.put_contact_information(wd)
+
+    def add_new_contact(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("add new").click()
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def put_contact_information(self, wd):
         wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Ivan")
         wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
         wd.find_element_by_name("middlename").send_keys("Ivanovich")
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("lastname").click()
