99e7e74fc368fa29a4c802e1f374bb2ae499fd07
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-06-26 16:11:17 +0300

    урок 2 улучшение читаемости

--- test_add_group.py
+++ test_add_group.py
@@ -17,20 +17,15 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd,username ="admin", password ="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="1", header="1", footer="1"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
          wd = self.wd
-         self.open_home_page(wd)
          self.login(wd, username="admin", password="secret")
          self.open_groups_page(wd)
          self.create_group(wd, Group(name="", header="", footer=""))
-         self.return_to_groups_page(wd)
          self.logout(wd)
 
     def logout(self, wd):
@@ -44,6 +39,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -58,7 +54,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_id("container").click()
+        self.return_to_groups_page(wd)
 
     def open_groups_page(self, wd):
         # open group page
@@ -66,6 +62,7 @@ class TestAddGroup(unittest.TestCase):
 
     def login(self, wd, username, password):
         # login
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
