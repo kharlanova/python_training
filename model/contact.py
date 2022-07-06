608d0d5bce82713909aea08689c717194a2a3005
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-07-06 12:31:54 +0300

    Исправление. Выделен класс для работы с контактами

--- model/contact.py
+++ model/contact.py
@@ -1,4 +1,4 @@
-class Contact_information:
+class Contact:
     def __init__(self,firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, email, email2, email3, bday, bmonth, byear, aday, amonth, ayear, address2, phone2,notes):
         self.firstname = firstname
         self.middlename = middlename
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from model.contact_information import Contact_information
+from model.contact import Contact
 from fixture.application import Application
 
 
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact_information(Contact_information(firstname="Ivan", middlename="Ivanovich", lastname="Petrov", nickname="iv", title="123", company="Petrov", address="Spb", home="8123456778", mobile="79117685656", work="79871234567", fax="123", email="iv@gmail.com", email2="vi@mail.com", email3="bn@mail.com", bday="1" , bmonth = "January", byear = "1980", aday = "9", amonth = "October", ayear = "2000", address2 = "Spb Fontanka", phone2 = "11",notes = "11"))
+    app.contact.contact_information(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Petrov", nickname="iv", title="123", company="Petrov", address="Spb", home="8123456778", mobile="79117685656", work="79871234567", fax="123", email="iv@gmail.com", email2="vi@mail.com", email3="bn@mail.com", bday="1" , bmonth = "January", byear = "1980", aday = "9", amonth = "October", ayear = "2000", address2 = "Spb Fontanka", phone2 = "11",notes = "11"))
     app.session.logout()
 
 
