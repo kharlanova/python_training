18ffde99762a7184f02c20707a150fb21bcc6288
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-07-28 19:05:21 +0300

    Задание 15,16

--- model/contact.py
+++ model/contact.py
@@ -1,6 +1,6 @@
 from sys import maxsize
 class Contact:
-    def __init__(self,firstname = None, middlename =None, lastname = None, nickname = None, title = None, company = None, address = None, homephone = None, mobilephone = None, workphone = None, fax = None, email = None, email2 = None, email3 = None, bday = None, bmonth = None, byear = None, aday = None, amonth = None, ayear = None, address2 = None, secondaryphone = None,notes = None, id=None,all_phones_from_home_page = None, all_emails_from_home_page = None):
+    def __init__(self,firstname = "", middlename ="", lastname = "", nickname = "", title = "", company = "", address = "", homephone = "", mobilephone = "", workphone = "", fax = "", email = "", email2 = "", email3 = "", bday = "", bmonth = "", byear = "", aday = "", amonth = "", ayear = "", address2 = "", secondaryphone = "",notes = "", id=None,all_phones_from_home_page =None, all_emails_from_home_page =None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -31,7 +31,7 @@ class Contact:
     def __repr__(self):
         return "%s:%s:%s" % (self.id,self.firstname,self.lastname)
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname.rstrip(" ") == other.firstname.rstrip(" ") and self.lastname.rstrip(" ") == other.lastname.rstrip(" ")
 
     def id_or_max(self):
         if self.id:
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,11 +2,22 @@
 
 import pytest
 from model.contact import Contact
+import random
+import string
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits  + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
+testdata = [Contact (firstname="", lastname="", address="")] + [
+        Contact(firstname=random_string("f", 10), lastname=random_string("l", 10), address=random_string("a", 20))
+        for i in range(5)
+    ]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app,contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Petrov", nickname="iv", title="123", company="Petrov", address="Spb", home="8123456778", mobile="79117685656", work="79871234567", fax="123", email="iv@gmail.com", email2="vi@mail.com", email3="bn@mail.com", bday="1" , bmonth = "January", byear = "1980", aday = "9", amonth = "October", ayear = "2000", address2 = "Spb Fontanka", phone2 = "11",notes = "11")
+    #contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Petrov", nickname="iv", title="123", company="Petrov", address="Spb", home="8123456778", mobile="79117685656", work="79871234567", fax="123", email="iv@gmail.com", email2="vi@mail.com", email3="bn@mail.com", bday="1" , bmonth = "January", byear = "1980", aday = "9", amonth = "October", ayear = "2000", address2 = "Spb Fontanka", phone2 = "11",notes = "11")
     app.contact.contact_information(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
