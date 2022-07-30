9473a1b5ea5cca6ada5e2b4e0743d4d5fc9a0781
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-07-30 12:44:49 +0300

    Задание 14 исправление

--- test/test_data_contact.py
+++ test/test_data_contact.py
@@ -7,15 +7,15 @@ def test_data_contact_on_home_page(app):
     index = randrange(len(contacts))
     contact_home = contacts[index]
     contact_edit = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_home.firstname == contact_edit.firstname
-    assert contact_home.lastname == contact_edit.lastname
-    assert contact_home.address == contact_edit.address
+    assert contact_home.firstname.rstrip(" ") == contact_edit.firstname.rstrip(" ")
+    assert contact_home.lastname.rstrip(" ") == contact_edit.lastname.rstrip(" ")
+    assert contact_home.address.rstrip(" ") == contact_edit.address.rstrip(" ")
     assert contact_home.all_emails_from_home_page == merge_emails_like_on_home_page(contact_edit)
     assert contact_home.all_phones_from_home_page == merge_phones_like_on_home_page(contact_edit)
 
 
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x:x is not None,
+    return "\n".join(filter(lambda x:x is not None and len(x) > 0,
                                        [contact.email, contact.email2, contact.email3]))
 
 def clear(s):
