db7d05f15dd765d3ba88005debd8d8ef4538ad74
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-08-03 18:17:07 +0300

    Исправление задания 17

--- conftest.py
+++ conftest.py
@@ -19,9 +19,9 @@ def app(request):
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
         with open(config_file) as f:
             target = json.load(f)
-        if fixture is None or not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=target['baseUrl'])
-        fixture.session.ensure_login(username=target['username'], password=target['password'])
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
