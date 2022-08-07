c481b0866a4bd8856d80308850b06c641e5e9f4b
kharlanova <107789471+kharlanova@users.noreply.github.com>
2022-08-07 13:55:57 +0300

    Задание 21 полная версия

---
+++ chek_db_connectoin.py
@@ -0,0 +1,11 @@
+from fixture.db import DbFixture
+
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+try:
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass
